3fb7d402ca45f7b36234769aed8c47b0a5c48ded
Killua.VX <killua.vx@gmail.com>
2013-09-05 20:42:00 +0800

    fix test with jenkins

--- webservice/webservice/test_settings.py
+++ webservice/webservice/test_settings.py
@@ -23,7 +23,6 @@ DATABASES['default'].update(dict(
 COVERAGE_EXCLUDES = EXTENDAL_APPS
 JENKINS_TASKS = (
     'django_jenkins.tasks.run_pylint',
-    'django_jenkins.tasks.run_sloccount',
     'django_jenkins.tasks.with_coverage',
     'django_jenkins.tasks.run_pep8',
     'django_jenkins.tasks.dir_tests',
