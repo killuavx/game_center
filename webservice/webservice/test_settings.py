cd4fe18503d047de2880c58e6b393650d442584d
Killua.VX <killua.vx@gmail.com>
2013-09-05 23:31:55 +0800

    fix test_settings remove django_jenkins.tasks.run_pep8

--- webservice/webservice/test_settings.py
+++ webservice/webservice/test_settings.py
@@ -24,7 +24,7 @@ COVERAGE_EXCLUDES = EXTENDAL_APPS
 JENKINS_TASKS = (
     'django_jenkins.tasks.run_pylint',
     'django_jenkins.tasks.with_coverage',
-    'django_jenkins.tasks.run_pep8',
+    'django_jenkins.tasks.dir_tests',
     'django_jenkins.tasks.django_tests',
 )
 
