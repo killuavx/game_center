426a4ecc4de862f11dde3370a507484987081c6d
Killua.VX <killua.vx@gmail.com>
2013-09-20 13:56:12 +0800

    指定mobapi search api的basename

--- webservice/mobapi/urls.py
+++ webservice/mobapi/urls.py
@@ -6,7 +6,7 @@ from searcher import views_rest as searcher_views_rest
 rest_router = routers.DefaultRouter()
 rest_router.register('authors', views.AuthorViewSet)
 rest_router.register('packages', views.PackageViewSet)
-rest_router.register('search', views.PackageSearchViewSet)
+rest_router.register('search', views.PackageSearchViewSet, base_name='search')
 rest_router.register('categories', views.CategoryViewSet)
 rest_router.register('topics', views.TopicViewSet)
 rest_router.register('tipswords', searcher_views_rest.TipsWordViewSet)
