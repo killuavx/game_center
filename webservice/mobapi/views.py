b039b2847a7731dcf1de5dd6bc3ef534df8e9d63
Killua.VX <killua.vx@gmail.com>
2013-11-04 11:54:05 +0800

    add selfupdate rest api description document

--- webservice/mobapi/views.py
+++ webservice/mobapi/views.py
@@ -1095,6 +1095,31 @@ from clientapp.models import ClientPackageVersion
 
 
 class SelfUpdateView(generics.RetrieveAPIView):
+    """ 客户端自更新接口
+
+    #### 访问方式
+
+        GET /api/selfupdate
+
+    #### 响应内容
+
+    * 200 HTTP_200_OK
+        * 获取成功, 返回客户端最后版本数据
+    * 204 HTTP_404_NOT_FOUND
+        * 没有可更新的数据
+
+    ### 返回数据结构
+
+    * `package_name`: 客户端的包名
+    * `version_code`: 最后版本的版本号
+    * `version_name`: 最后版本的版本名
+    * `whatsnew`: 更新描述内容
+    * `summary`: 一句话摘要
+    * `download`: 客户端下载地址
+    * `download_size`: 文件字节大小
+
+    ----
+    """
 
     serializer_class = ClientPackageVersionSerializer
     queryset = ClientPackageVersion.objects.published()
