edafb73eead35c0f07b22538761a63fd6b029594
Killua.VX <killua.vx@gmail.com>
2014-04-18 16:20:49 +0800

    fix mongo configuarion port

--- webservice/webservice/module_settings/mongo_env.py
+++ webservice/webservice/module_settings/mongo_env.py
@@ -3,12 +3,12 @@ import os
 MOGOENGINE_CONNECTS = {
     'default': {
         'host': os.getenv('MONGODB_HOST_DW', 'localhost'),
-        'port': os.getenv('MONGODB_PORT_DW', 27017),
+        'port': int(os.getenv('MONGODB_PORT_DW', 27017)),
         'name': os.getenv('MONGODB_NAME_DW', 'datawarehouse'),
         },
     'systemresource': {
         'host': os.getenv('MONGODB_HOST_SR', 'localhost'),
-        'port': os.getenv('MONGODB_PORT_SR', 27017),
+        'port': int(os.getenv('MONGODB_PORT_SR', 27017)),
         'name': os.getenv('MONGODB_NAME_SR', 'systemresource'),
         }
 }
