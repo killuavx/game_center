826917590a912372aed323a8725e5bdae06d8ce2
Killua.VX <killua.vx@gmail.com>
2014-04-24 21:09:23 +0800

    fix mobapi2.urls viewname with prefix "apiv2-"

--- webservice/mobapi2/urls.py
+++ webservice/mobapi2/urls.py
@@ -15,8 +15,38 @@ from mobapi2.promotion.views import AdvertisementViewSet
 from mobapi2.account.views import PackageBookmarkViewSet
 from mobapi2.comment.views import CommentViewSet
 from mobapi2.warehouse.views.packageversion import PackageVersionViewSet
+from mobapi2.account.views import (AccountCreateView,
+                                   AccountMyProfileView,
+                                   AccountSignoutView,
+                                   AccountAuthTokenView,
+                                   AccountCommentPackageView)
+from analysis.views.rest_views import EventViewSet
 
-rest_router = routers.DefaultRouter()
+
+class ApiVersionRouter(routers.DefaultRouter):
+
+    prefix = 'api'
+
+    version_prefix = 'v2'
+
+    def __init__(self, version, trailing_slash=True):
+        super(ApiVersionRouter, self).__init__(trailing_slash=trailing_slash)
+        self.version_prefix = version
+
+    def register(self, prefix, viewset, base_name=None):
+        if base_name is not None:
+            base_name = self.get_base_name(base_name)
+        super(ApiVersionRouter, self).register(prefix=prefix, viewset=viewset, base_name=base_name)
+
+    def get_default_base_name(self, viewset):
+        name = super(ApiVersionRouter, self).get_default_base_name(viewset)
+        return self.get_base_name(name)
+
+    def get_base_name(self, base_name):
+        return "-".join([self.prefix+self.version_prefix, base_name])
+
+
+rest_router = ApiVersionRouter('v2')
 rest_router.register('authors', AuthorViewSet)
 rest_router.register('packages', PackageViewSet)
 rest_router.register('packageversions', PackageVersionViewSet)
@@ -26,24 +56,40 @@ rest_router.register('categories', CategoryViewSet)
 rest_router.register('topics', TopicViewSet)
 rest_router.register('tipswords', TipsWordViewSet)
 rest_router.register('advertisements', AdvertisementViewSet)
-rest_router.register('bookmarks',
-                     PackageBookmarkViewSet,
-                     base_name='bookmark')
+rest_router.register('bookmarks', PackageBookmarkViewSet, base_name='bookmark')
 rest_router.register('comments', CommentViewSet)
-
-from analysis.views.rest_views import EventViewSet
 rest_router.register('events', EventViewSet, base_name='event')
 
 urlpatterns = rest_router.urls
 
 from mobapi2.clientapp.views import SelfUpdateView
-urlpatterns += patterns('',
-    url(r'^accounts/', include('mobapi2.account.urls')),
-    url(r'^selfupdate/?$', SelfUpdateView.as_view(),
-        name='selfupdate',
-        prefix='selfupdate'),
-    url(r'^push/packages/?$', PackagePushView.as_view(), name='push-packages', prefix='push'),
 
-    url(r'^updates/?$', PackageUpdateView.as_view(), name='update-create', prefix='update'),
+def _account_basename(name):
+    prefix='account'
+    basename = "%s-%s" %(prefix, name)
+    return rest_router.get_base_name(basename)
+
+account_urlpatterns = patterns('',
+                       url(r'^signup/?$', AccountCreateView.as_view(),
+                           name=_account_basename('signup')),
+                       url(r'^signin/?$', AccountAuthTokenView.as_view(),
+                           name=_account_basename('signin')),
+                       url(r'^signout/?$', AccountSignoutView.as_view(),
+                           name=_account_basename('signout')),
+                       url(r'^myprofile/?$', AccountMyProfileView.as_view(),
+                           name=_account_basename('myprofile')),
+                       url(r'^commented_packages/?$',
+                           AccountCommentPackageView.as_view(),
+                           name=_account_basename('commentedpackages')),
+                       )
+
+urlpatterns += patterns('',
+    url(r'^selfupdate/?$', SelfUpdateView.as_view(),
+        name=rest_router.get_base_name('selfupdate')),
+    url(r'^push/packages/?$', PackagePushView.as_view(),
+        name=rest_router.get_base_name('push-packages')),
+    url(r'^updates/?$', PackageUpdateView.as_view(),
+        name=rest_router.get_base_name('update-create')),
+    url(r'^accounts/', include(account_urlpatterns))
 )
 
