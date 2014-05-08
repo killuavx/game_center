9506d54d9eb63ccbbfb8f4c26d531436c9472c85
Killua.VX <killua.vx@gmail.com>
2014-05-08 21:18:45 +0800

    tiny fix migration for PackageVersion subtitle/summary/description

--- webservice/warehouse/migrations/0020_update_packageversion_subtitle_summary_description.py
+++ webservice/warehouse/migrations/0020_update_packageversion_subtitle_summary_description.py
@@ -7,6 +7,7 @@ from django.db import models
 class Migration(DataMigration):
 
     def forwards(self, orm):
+        """
         for pv in orm['warehouse.PackageVersion']._default_manager.all():
             try:
                 package = pv.package
@@ -16,6 +17,7 @@ class Migration(DataMigration):
                 pv.save()
             except:
                 pass
+        """
 
     def backwards(self, orm):
         "Write your backwards methods here."
