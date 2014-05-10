41f257afe641b964b936a04cc9034452a28350f0
Sheng <webmaster0115@gmail.com>
2014-05-10 15:56:36 +0800

    added TopicPackageListBoxWidget

--- webservice/website/widgets/ios_pc.py
+++ webservice/website/widgets/ios_pc.py
@@ -2,6 +2,7 @@
 from django_widgets import Widget
 from .common.promotion import BaseSingleAdvWidget, BaseMultiAdvWidget
 from .masterpiece import MasterpiecePackageListWidget
+from .common.topic import BaseTopicPackageListWidget
 
 
 class BannerToprightWidget(BaseSingleAdvWidget, Widget):
@@ -13,7 +14,15 @@ class BannerTopleftWidget(BaseMultiAdvWidget, Widget):
 
     template = 'pages/widgets/ios_pc/banner_top_left.html'
 
+
 class PackageListBoxWidget(MasterpiecePackageListWidget):
 
     template = 'pages/widgets/ios_pc/roll_box.html'
 
+
+class TopicPackageListBoxWidget(BaseTopicPackageListWidget):
+
+    template = 'pages/widgets/ios_pc/package_list_box_left.html'
+
+#    def get_context(self, value=None, options=dict(), context=None):
+#        pass
