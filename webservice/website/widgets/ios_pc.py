1a0d4afa9e11859390b575a2c96d8fa2fe247b9a
Sheng <webmaster0115@gmail.com>
2014-05-11 18:50:55 +0800

    modified TopicPackageListBoxWidget

--- webservice/website/widgets/ios_pc.py
+++ webservice/website/widgets/ios_pc.py
@@ -3,8 +3,6 @@ from django_widgets import Widget
 from .common.promotion import BaseSingleAdvWidget, BaseMultiAdvWidget
 from .masterpiece import MasterpiecePackageListWidget
 from .common.topic import BaseTopicPackageListWidget
-from .common.package import BaseRankingPackageListWidget
-
 
 
 class BannerToprightWidget(BaseSingleAdvWidget, Widget):
@@ -26,7 +24,26 @@ class TopicPackageListBoxWidget(BaseTopicPackageListWidget):
 
     template = 'pages/widgets/ios_pc/package_list_box_left.html'
 
+    def get_context(self, value=None, options=dict(), context=None):
+        #print (options['slugs'])
+        slugs =  options.get('slugs', None)
+        type = options.get('type', None)
+        result = []
+        if slugs:
+            slug_lst = slugs.split('|')
+            for slug in slug_lst:
+                options['slug'] = slug
+                tmp = super(TopicPackageListBoxWidget, self).get_context(value, options, context)
+                result.append(tmp)
 
-class IosPcRankingPackageListWidget(BaseRankingPackageListWidget):
+        #print ({'result': result})
+        return {'result': result, 'type': type}
 
-    template = 'pages/widgets/ios_pc/package_list_box_right.html'
+
+#class IosPcRankingPackageListWidget(BasePackageListWidget):
+#
+##    def get_list(self):
+##        PackageRanking.objects.
+#
+#
+#    template = 'pages/widgets/ios_pc/package_list_box_right.html'
