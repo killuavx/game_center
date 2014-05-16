875ca3ba858942e76b15fef2da79514b7df81ca4
Sheng <webmaster0115@gmail.com>
2014-05-16 17:33:41 +0800

    changed route name

--- webservice/website/ios_pc_urls.py
+++ webservice/website/ios_pc_urls.py
@@ -7,5 +7,5 @@ slug_pattern = '[\w_.-]+'
 urlpatterns = patterns('website.views',
     url(r'package/(?P<package_name>%s)/$' % (slug_pattern),'iospc_package_detail_views', name='iospc_package_detail'),
     url(r'packages/(?P<cat_slug>%s)/(?P<other_slug>\w+)/' % slug_pattern, 'iospc_packages_topic_list_views', name='iospc_packages_topic_list'),
-    url(r'packages/(?P<slug>%s)/' % slug_pattern, 'iospc_packages_cat_list_views', name='iospc_packages_list'),
+    url(r'packages/(?P<slug>%s)/' % slug_pattern, 'iospc_packages_cat_list_views', name='iospc_packages_cat_list'),
 )
