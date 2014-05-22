1e61583534805bebc4a4ef862d037fce7bc4513f
Killua.VX <killua.vx@gmail.com>
2014-05-22 21:03:44 +0800

    fix ResourceInlines readonly to  check has_change_permission

--- webservice/toolkit/admin.py
+++ webservice/toolkit/admin.py
@@ -55,9 +55,6 @@ class ResourceInlines(GenericStackedInline):
     ordering = ('kind', 'alias', )
     readonly_fields = ('file_size', 'file_md5')
 
-    def get_readonly_fields(self, request, obj=None):
-        fields = self.readonly_fields
-        if obj and obj.pk:
-            fields = self.fields
-        return fields
+    def has_change_permission(self, request, obj=None):
+        return False
 
