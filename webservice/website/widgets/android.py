ad04282e5e99764a70d400ff8eab7d5669e8189e
Sheng <webmaster0115@gmail.com>
2014-05-23 17:54:04 +0800

    added masterpiece widget

--- webservice/website/widgets/android.py
+++ webservice/website/widgets/android.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 from django_widgets import Widget
 from .common.promotion import BaseMultiAdvWidget
+from .masterpiece import MasterpiecePackageListWidget
 
 
 class HomeTopBannersWidget(BaseMultiAdvWidget, Widget):
 
    template='pages/widgets/android/home-top-banner.html'
+
+class HomeMasterpiecePackageListWidget(MasterpiecePackageListWidget):
+
+    per_page = 10
+    template = 'pages/widgets/android/masterpiece.html'
+
