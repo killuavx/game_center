4a533ae7181238dcfa4eb6bda31f2fd373d8032a
Killua.VX <killua.vx@gmail.com>
2014-05-23 10:04:26 +0800

    mobapi2 clientapp下的接口配置缓存

--- webservice/mobapi2/cache_keyconstructors.py
+++ webservice/mobapi2/cache_keyconstructors.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.utils.encoding import force_text
-from rest_framework_extensions.key_constructor import (bits,
-                                                       constructors)
-
-
+from rest_framework_extensions.key_constructor import (bits, constructors)
+from django.core.cache import cache
+from django.utils.timezone import now
+from toolkit.helpers import current_site_id
 
 
 class OrderingKeyBit(bits.QueryParamsKeyBit):
@@ -29,3 +29,86 @@ class LookupListKeyConstructor(constructors.DefaultKeyConstructor):
 
 class LookupOrderingListKeyConstructor(LookupListKeyConstructor):
     ordering = OrderingKeyBit()
+
+
+class ContentObjectKeyBit(bits.QueryParamsKeyBit):
+
+    def get_data(self, **kwargs):
+        kwargs['params'] = []
+        kwargs['params'].append('content_type')
+        kwargs['params'].append('object_pk')
+        return super(ContentObjectKeyBit, self).get_data(**kwargs)
+
+
+class ContentObjectListKeyConstructor(constructors.DefaultKeyConstructor):
+
+    content_object = ContentObjectKeyBit()
+
+    pagination = bits.PaginationKeyBit()
+
+
+class PackageFilterKeyBit(bits.QueryParamsKeyBit):
+
+    def get_data(self, **kwargs):
+        kwargs['params'] = []
+        kwargs['params'].append('package_name')
+        kwargs['params'].append('version_name')
+        return super(PackageFilterKeyBit, self).get_data(**kwargs)
+
+
+class PackageFilterKeyConstructor(constructors.DefaultKeyConstructor):
+
+    package = PackageFilterKeyBit()
+
+
+class PackageLookupKeyBit(bits.KeyBitBase):
+
+    def get_data(self, **kwargs):
+        _kwargs = kwargs.get('kwargs')
+        p = _kwargs.get('package_name')
+        v = _kwargs.get('version_name')
+        p = p if p else ''
+        v = v if v else ''
+        pv_txt = force_text(":".join([p,v]))
+        return pv_txt
+
+
+class PackageLookupConstructor(constructors.DefaultKeyConstructor):
+
+    package = PackageLookupKeyBit()
+
+
+def make_cache_key(prefix):
+    return "%s.%s.%s" %('api', current_site_id(), prefix)
+
+
+class UpdatedAtKeyBit(bits.KeyBitBase):
+
+    content_type = 'default'
+
+    def __init__(self, content_type=None, params=None):
+        super(UpdatedAtKeyBit, self).__init__(params)
+        if content_type:
+            self.content_type = content_type
+
+    def get_key(self):
+        return make_cache_key('updated_at_timestamp.%s' % self.content_type)
+
+    def get_data(self, **kwargs):
+        key = self.get_key()
+        value = cache.get(key, None)
+        if not value:
+            value = now()
+            cache.set(key, value=value)
+        return force_text(value)
+
+
+class ContentObjectUpdatedAtContructor(constructors.DefaultKeyConstructor):
+
+    content_object = ContentObjectKeyBit()
+
+    updated_at = UpdatedAtKeyBit()
+
+    def __init__(self, content_type=None, **kwargs):
+        super(ContentObjectUpdatedAtContructor, self).__init__(**kwargs)
+        self.updated_at = UpdatedAtKeyBit(content_type=content_type)
