c855769afa4ee1473a94fc4b12c90a24c421f264
Sheng <webmaster0115@gmail.com>
2014-05-23 15:25:51 +0800

    added db_local.py

---
+++ webservice/webservice/module_settings/db_local.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv('DBA_SQL_DJANGO_ENGINE', 'django.db.backends.postgresql_psycopg2'),
+        'NAME': os.getenv('DBA_SQL_DB_NAME', 'webservice'),
+        'USER': os.getenv('DBA_SQL_ADMIN_USER', 'webservice'),
+        'PASSWORD': os.getenv('DBA_SQL_ADMIN_PASSWORD', '123456'),
+        'HOST': os.getenv('DBA_SQL_HOST', '127.0.0.1'),
+        'PORT': os.getenv('DBA_SQL_PORT', '5432'),
+    },
+    'datawarehouse': {
+        'ENGINE': os.getenv('DBA_SQL_DJANGO_ENGINE_DW', 'django.db.backends.postgresql_psycopg2'),
+        'NAME': os.getenv('DBA_SQL_DB_NAME_DW', 'datawarehouse'),
+        'USER': os.getenv('DBA_SQL_ADMIN_USER_DW', 'datawarehouse'),
+        'PASSWORD': os.getenv('DBA_SQL_ADMIN_PASSWORD_DW', ''),
+        'HOST': os.getenv('DBA_SQL_HOST_DW', '127.0.0.1'),
+        'PORT': os.getenv('DBA_SQL_PORT_DW', '5432'),
+        'OPTIONS': {'autocommit': True,}
+    }
+}
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': 'webservice-database.sqlite',
+#        'USER': '',
+#        'PASSWORD': '',
+#        'HOST': '',
+#        'PORT': '',
+#        },
+#    'datawarehouse': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': 'webservice-database.sqlite',
+#        'USER': '',
+#        'PASSWORD': '',
+#        'HOST': '',
+#        'PORT': '',
+#        }
+#}
