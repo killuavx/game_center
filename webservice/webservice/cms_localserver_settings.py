19f3e29d397ad23a3e15d87f6bd0fd850a13e978
Sheng <webmaster0115@gmail.com>
2014-05-23 15:24:58 +0800

    added android static pages

--- webservice/webservice/cms_localserver_settings.py
+++ webservice/webservice/cms_localserver_settings.py
@@ -9,7 +9,8 @@ DEBUG = TEMPLATE_DEBUG = True
 import os
 HOST_URL = os.getenv('GC_HOST_URL', '')
 MEDIA_URL = 'http://media.ccplay.com.cn/%s/' % MEDIA_URL.strip('/')
-STATIC_URL = 'http://static.ccplay.com.cn/%s/'% STATIC_URL.strip('/')
+#STATIC_URL = 'http://static.ccplay.com.cn/%s/'% STATIC_URL.strip('/')
+STATIC_URL = 'http://gc.ccplay.com.cn/%s/'% STATIC_URL.strip('/')
 PUBLISH_MEDIA_URL = MEDIA_URL
 PUBLISH_STATIC_URL = STATIC_URL
 
--- webservice/webservice/cms_settings.py
+++ webservice/webservice/cms_settings.py
@@ -89,6 +89,7 @@ PAGE_MENU_TEMPLATES = (
     (1, "Top navigation bar", "pages/menus/header.haml"),
     (2, "Left-hand tree", "pages/menus/tree.html"),
     (3, "Footer", "pages/menus/footer_links.haml"),
+    (4, "Android navigation menu", "pages/menus/android/navigation.html"),
 )
 
 replace_idx = INTERNAL_APPS.index('suit')
