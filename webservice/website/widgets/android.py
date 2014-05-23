e4a2f341ce0883535d72f798e38f97f9d5565c68
Sheng <webmaster0115@gmail.com>
2014-05-23 21:55:17 +0800

    added left-tab-box.html template

--- webservice/website/widgets/android.py
+++ webservice/website/widgets/android.py
@@ -4,12 +4,40 @@ from .common.promotion import BaseMultiAdvWidget
 from .common.base import BaseListWidget
 from .masterpiece import MasterpiecePackageListWidget
 from warehouse.models import Package
-from taxonomy.models import Category
+from taxonomy.models import Category, Topic, TopicalItem
+from .common.topic import BaseTopicPackageListWidget
+
+
+def get_root_category_by_slug(slug):
+    try:
+        root_cat = Category.objects.get(slug=slug)
+    except:
+        root_cat = None
+
+    return root_cat
+
+def filter_packages_by_category_slug(packages, slug):
+
+    root_cat = get_root_category_by_slug(slug)
+
+    if root_cat is None:
+        return []
+
+    cats = root_cat.get_descendants(True)
+    pkgs =  packages.filter(categories__in=cats)
+    if not pkgs:
+        return []
+
+    return  pkgs.distinct().by_published_order()
 
 
 class HomeTopBannersWidget(BaseMultiAdvWidget, Widget):
 
    template='pages/widgets/android/home-top-banner.html'
+
+class HomeTopAdWidget(BaseMultiAdvWidget, Widget):
+
+   template='pages/widgets/android/home-top-ad.html'
 
 
 class HomeMasterpiecePackageListWidget(MasterpiecePackageListWidget):
@@ -21,6 +49,7 @@ class HomeMasterpiecePackageListWidget(MasterpiecePackageListWidget):
 class LatestPackageListWidget(BaseListWidget):
 
     template = 'pages/widgets/android/latest-release.html'
+    cat = None
     slug = None
 
     def get_list(self):
@@ -30,6 +59,7 @@ class LatestPackageListWidget(BaseListWidget):
             return []
 
         cats = cat.get_descendants(True)
+        self.cat = cat
 
         try:
             packages = Package.objects.filter(categories__in=cats).\
@@ -42,6 +72,65 @@ class LatestPackageListWidget(BaseListWidget):
     def get_context(self, value=None, options=dict(), context=None):
         self.slug = options.get('slug', None)
         packages = self.get_list()
-        options.update(items=packages)
+        options.update(
+            items=packages,
+            cat=self.cat,
+        )
         #print(packages.count())
         return options
+
+
+class CategorizedPackageListWidget(LatestPackageListWidget):
+
+    template = 'pages/widgets/android/first-publish-crack.html'
+
+
+class HomeTopicsPackageListWidget(BaseTopicPackageListWidget):
+
+    template = 'pages/widgets/android/selected-webgames.html'
+
+
+class HomeTabsPackageListWidget(BaseTopicPackageListWidget):
+
+    template = 'pages/widgets/android/left-tab-box.html'
+    cat = None
+
+    def get_packages_by_category_slug(self, packages, slug):
+        return filter_packages_by_category_slug(packages, slug)
+
+    def get_packages_by_topic_slug(self, packages, slug):
+        tmp = {}
+        if slug == 'latest_published':
+            tmp['items'] =  packages.by_published_order()
+            tmp['topic_name']  = '最新发布'
+        else:
+            try:
+                topic = Topic.objects.filter(slug=slug).published().get()
+                print (slug)
+                print (topic)
+                tmp['items'] = TopicalItem.objects.filter_items_by_topic(topic, Package, packages)
+                tmp['topic_name']  = topic.name
+            except:
+                pass
+
+        #print (tmp)
+        return tmp
+
+
+    def get_context(self, value=None, options=dict(), context=None):
+        slugs =  options.get('slugs', None)
+        cat  = options.get('cat', None)
+        cat_dic = {
+            'game': '游戏',
+            'application': '软件',
+        }
+        packages = self.get_packages_by_category_slug(Package.objects.all(), cat)
+        result = []
+
+        if slugs and packages:
+            for slug in slugs.split('|'):
+                result.append(self.get_packages_by_topic_slug(packages, slug))
+
+        #print ({'result': result})
+        return {'result': result, 'cat': cat_dic.get(cat)}
+
