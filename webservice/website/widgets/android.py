4a87d3769d212a2ee522b128e15ab12ece79f585
Sheng <webmaster0115@gmail.com>
2014-05-24 10:49:26 +0800

    added HomeRankingPackageListWidget

--- webservice/website/widgets/android.py
+++ webservice/website/widgets/android.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from django_widgets import Widget
-from .common.promotion import BaseMultiAdvWidget
-from .common.base import BaseListWidget
-from .masterpiece import MasterpiecePackageListWidget
 from warehouse.models import Package
 from taxonomy.models import Category, Topic, TopicalItem
+from .common.promotion import BaseMultiAdvWidget
+from .common.base import BaseListWidget
 from .common.topic import BaseTopicPackageListWidget
+from .common.package import BaseRankingPackageListWidget
+from .masterpiece import MasterpiecePackageListWidget
 
 
 def get_root_category_by_slug(slug):
@@ -111,7 +112,7 @@ class HomeTabsPackageListWidget(BaseTopicPackageListWidget):
             except:
                 pass
 
-        print (tmp)
+        #print (tmp)
         return tmp
 
 
@@ -130,6 +131,26 @@ class HomeTabsPackageListWidget(BaseTopicPackageListWidget):
                 result.append(self.get_packages_by_topic_slug(packages, slug))
 
         #print ({'result': result})
-        print (len(result))
+        #print (len(result))
         return {'result': result, 'cat': cat_dic.get(cat)}
+
+
+class HomeRankingPackageListWidget(BaseRankingPackageListWidget):
+
+    template = 'pages/widgets/android/right-ranking-box.html'
+    cat = None
+
+    def get_list(self):
+        from ranking.models import PackageRanking
+        pkgRks = PackageRanking.objects.filter(cycle_type=0).filter(ranking_type__slug="total").filter(category__slug=self.cat)
+        if not pkgRks:
+            return []
+        else:
+            return pkgRks[0].packages.all()
+
+    def get_context(self, value=None, options=dict(), context=None):
+        self.cat = options.get('cat', None)
+        items = self.get_list()
+        print(len(items))
+        return {'items': items, 'cat': self.cat}
 
