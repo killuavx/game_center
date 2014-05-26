33351506a998611207494f692e1308708a9592d8
Killua.VX <killua.vx@gmail.com>
2014-05-26 11:16:14 +0800

    tiny change

--- webservice/website/defaults.py
+++ webservice/website/defaults.py
@@ -29,9 +29,15 @@ register_setting(
 
 register_setting(
     name="GC_TOPICS_MASTERPIECE_SLUG",
-    description=_("Topic.slug, 巨作页面，当进入/masterpiece/，指定一个专辑(topic)展示游戏列表" ),
+    description=_("Topic.slug, 巨作页面，指定一个专辑(topic)展示游戏列表" ),
     editable=True,
     default='masterpiece',
+    )
+register_setting(
+    name="GC_TOPICS_VENDOR_SLUG",
+    description=_("Topic.slug, 顶级开发商指定一个专辑(topic)展示warehouse.author列表" ),
+    editable=True,
+    default='spec-top-author',
     )
 
 register_setting(
