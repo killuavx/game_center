b672ab4c4a4462d6f7df02cdb11d703a51ad5eee
jun <jun.gong@ccplay.com.cn>
2014-05-28 14:50:40 +0800

    Signed-off-by: jun <jun.gong@ccplay.com.cn>

---
+++ webservice/fontend-static/cc_web/js/down.js
@@ -0,0 +1,14 @@
+var animate=function(){
+	var topsize=$(window).scrollTop();
+	if(topsize>=300){
+		$(".d-1 img").addClass("animate")
+		}
+	if(topsize>=800){
+		$(".d-2 img").addClass("animate")
+		}
+	if(topsize>=820){
+		_value=3;
+		$(".d-3 img").addClass("animate")
+		}
+	}
+$(window).bind("scroll resize",animate);
