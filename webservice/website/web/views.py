11944d43607c7063e8574fbbe1fef0a7c9bc519d
Killua.VX <killua.vx@gmail.com>
2014-06-08 14:03:54 +0800

    qr fix

--- webservice/website/web/views.py
+++ webservice/website/web/views.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
+from datetime import timedelta
+from django.utils.timezone import is_aware, make_aware, UTC
+import os
 from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import Http404, HttpResponse
 from django.shortcuts import redirect
 from django.template.response import TemplateResponse
+from django.utils.timezone import now, get_default_timezone
 from taxonomy.models import Category
 from website.views import base
 from os.path import join
@@ -105,18 +109,42 @@ def _get_qrcode(*args):
     qr.make(fit=True)
     return qr.make_image()
 
-def _qrcode_cdn_publish_one(relative_file):
+
+def _qrcode_cdn_publish_one_and_get_image_url(relative_file, created=True):
     from website.cdn.processors.base import StaticProcessor
     from website.documents.cdn import SyncQueue
-    if not SyncQueue.objects.filter(resource_path=relative_file).count():
+
+    if created and not SyncQueue.objects.filter(resource_path=relative_file).count():
         processor = StaticProcessor(content_type=StaticProcessor.CONTENT_TYPE_MEDIA,
                                     relative_path='qr')
         processor.publish_one(relative_file)
 
-    if settings.DEBUG:
+    is_published_ok = False
+    try:
+        sync_queue = SyncQueue.objects.filter(resource_path=relative_file).get()
+        if sync_queue.latest_fb_result == 'SUCCESS':
+            is_published_ok = True
+    except:
+        pass
+
+    if created or is_published_ok:
         return '/%s/%s' % (StaticProcessor.CONTENT_TYPE_MEDIA, relative_file)
 
     return join(settings.MEDIA_URL, relative_file)
+
+
+def _qrcode_get_or_save_file_to_relative_path(img, hash_qr):
+    relative_file = join('qr', '%s.png' % hash_qr)
+    qr_fp = join(settings.MEDIA_ROOT, relative_file)
+
+    if os.path.isfile(qr_fp):
+        return relative_file, False
+
+    with open(qr_fp, 'wb') as f:
+        img.save(f)
+
+    return relative_file, True
+
 
 def qrcode_gen(request, *args, **kwargs):
     url = request.GET.get('url')
@@ -125,13 +153,8 @@ def qrcode_gen(request, *args, **kwargs):
 
     hash_qr = _hash_data(url)
     img = _get_qrcode(url)
-
-    relative_file = join('qr', '%s.png' % hash_qr)
-    qr_fp = join(settings.MEDIA_ROOT, relative_file)
-    with open(qr_fp, 'wb') as f:
-        img.save(f)
-
-    image_url = _qrcode_cdn_publish_one(relative_file=relative_file)
+    relative_file, created = _qrcode_get_or_save_file_to_relative_path(img, hash_qr)
+    image_url = _qrcode_cdn_publish_one_and_get_image_url(relative_file=relative_file)
     return redirect(to=image_url)
 
 
