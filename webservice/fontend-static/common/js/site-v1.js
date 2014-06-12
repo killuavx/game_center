f5d12c5b805518ab7fd0b5d3941fd59dd1bb9987
jun <jun.gong@ccplay.com.cn>
2014-06-13 04:50:13 +0800

    Signed-off-by: jun <jun.gong@ccplay.com.cn>

--- webservice/fontend-static/common/js/site-v1.js
+++ webservice/fontend-static/common/js/site-v1.js
@@ -93,8 +93,8 @@ $(document).ready(function(){
 $(function(){
 
 //新窗口打开链接	
-$(".app-list-m a,.app-list-xl a,.app-list-l a,.app-list-min a,.hot-bbs-list a,.novice-bbs-list a,.i-link-list a,#home a,.hot-game a,.focusBox_pic a,.h-app a,.cc-a-d-box a,.h-bbs a,.top-list a,.footer a").attr("target","_blank");
-$(".btn-s a,.user-switch a,.i-5,.f-logo").attr("target","");
+$(".app-list-m a,.app-list-xl a,.app-list-l a,.app-list-min a,.hot-bbs-list a,.novice-bbs-list a,.i-link-list a,#home a,.hot-game a,.focusBox_pic a,.h-app a,.cc-a-d-box a,.h-bbs a,.top-list a,.masterpiece a").attr("target","_blank");
+$(".btn-s a,.user-switch a,.i-5,.f-logo,.collection_box .l a").attr("target","");
 
 
 //搜索
@@ -670,41 +670,4 @@ $(document).ready(function(){
 		}
 		return $width;
 	}
-});
-
-$(document).ajaxSend(function(event, xhr, settings) {
-    function getCookie(name) {
-        var cookieValue = null;
-        if (document.cookie && document.cookie != '') {
-            var cookies = document.cookie.split(';');
-            for (var i = 0; i < cookies.length; i++) {
-                var cookie = jQuery.trim(cookies[i]);
-                // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
-            }
-        }
-        return cookieValue;
-    }
-    function sameOrigin(url) {
-        // url could be relative or scheme relative or absolute
-        var host = document.location.host; // host + port
-        var protocol = document.location.protocol;
-        var sr_origin = '//' + host;
-        var origin = protocol + sr_origin;
-        // Allow absolute or scheme relative URLs to same origin
-        return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
-            (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
-            // or any other URL that isn't scheme relative or absolute i.e relative.
-            !(/^(\/\/|http:|https:).*/.test(url));
-    }
-    function safeMethod(method) {
-        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-    }
-
-    if (!safeMethod(settings.type) && sameOrigin(settings.url)) {
-        xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-    }
-});
+});	
