61b3c476b897390f80195a726257617a83a10812
Killua.VX <killua.vx@gmail.com>
2014-06-15 18:12:26 +0800

    mobapi2 warehouse.views.author添加etag/cache_control控制

--- webservice/mobapi2/warehouse/views/author.py
+++ webservice/mobapi2/warehouse/views/author.py
@@ -6,12 +6,13 @@ from mobapi2.warehouse.serializers.author import AuthorSerializer
 from mobapi2.warehouse.views.package import PackageViewSet
 from rest_framework_extensions.cache.decorators import cache_response
 from mobapi2 import cache_keyconstructors as ckc
-from rest_framework_extensions.cache.mixins import CacheResponseMixin
+from rest_framework_extensions.utils import default_list_cache_key_func, default_object_cache_key_func
 from mobapi2.warehouse.views.filters import TopicalAuthorFilter
+from mobapi2.decorators import default_cache_control
+from rest_framework_extensions.etag.decorators import etag
 
 
-class AuthorViewSet(CacheResponseMixin,
-                    viewsets.ReadOnlyModelViewSet):
+class AuthorViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = AuthorSerializer
     filter_backends = (TopicalAuthorFilter, )
 
@@ -22,6 +23,11 @@ class AuthorViewSet(CacheResponseMixin,
             self.queryset = self.model.objects.all()
         return self.queryset
 
+    packages_key_func = ckc.LookupOrderingListKeyConstructor()
+
+    @etag(packages_key_func)
+    @cache_response(key_func=packages_key_func)
+    @default_cache_control()
     @link()
     def packages(self, request, pk, *args, **kwargs):
         author = generics.get_object_or_404(self.get_queryset(), pk=pk)
@@ -29,3 +35,17 @@ class AuthorViewSet(CacheResponseMixin,
         queryset = author.packages.all()
         list_view = ViewSet.as_view({'get': 'list'}, queryset=queryset)
         return list_view(request, *args, **kwargs)
+
+
+    @etag(default_list_cache_key_func)
+    @cache_response(key_func=default_list_cache_key_func)
+    @default_cache_control()
+    def list(self, request, *args, **kwargs):
+        return super(AuthorViewSet, self).list(request, *args, **kwargs)
+
+
+    @etag(default_object_cache_key_func)
+    @cache_response(key_func=default_object_cache_key_func)
+    @default_cache_control()
+    def retrieve(self, request, *args, **kwargs):
+        return super(AuthorViewSet, self).retrieve(request, *args, **kwargs)
