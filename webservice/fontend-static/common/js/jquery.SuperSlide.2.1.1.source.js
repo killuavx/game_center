96fe5117a30014c25e58b3ad7e5a0d49575e5060
jun <jun.gong@ccplay.com.cn>
2014-06-24 18:06:37 +0800

    Signed-off-by: jun <jun.gong@ccplay.com.cn>

--- webservice/fontend-static/common/js/jquery.SuperSlide.2.1.1.source.js
+++ webservice/fontend-static/common/js/jquery.SuperSlide.2.1.1.source.js
@@ -169,11 +169,11 @@
 				{
 					case "fold": conBox.css({"position":"relative","width":slideW,"height":slideH}).children().css( {"position":"absolute","width":selfW,"left":0,"top":0,"display":"none"} ); break;
 					case "top": conBox.wrap('<div class="tempWrap" style="overflow:hidden; position:relative; height:'+vis*slideH+'px"></div>').css( { "top":-(index*scroll)*slideH, "position":"relative","padding":"0","margin":"0"}).children().css( {"height":selfH} ); break;
-					case "left": conBox.wrap('<div class="tempWrap" style="overflow:hidden; position:relative; width:'+vis*slideW+'px"></div>').css( { "width":conBoxSize*slideW,"left":-(index*scroll)*slideW,"position":"relative","overflow":"hidden","padding":"0","margin":"0"}).children().css( {"float":"left"} ); break; //,"width":selfW
+					case "left": conBox.wrap('<div class="tempWrap" style="overflow:hidden; position:relative; width:'+vis*slideW+'px"></div>').css( { "width":conBoxSize*slideW,"left":-(index*scroll)*slideW,"position":"relative","overflow":"hidden","padding":"0","margin":"0"}).children().css( {"float":"left","width":selfW} ); break; //,"width":selfW
 					case "leftLoop":
 					case "leftMarquee":
 						cloneEle();
-						conBox.wrap('<div class="tempWrap" style="overflow:hidden; position:relative;></div>').css( { "width":(conBoxSize+vis+cloneNum)*slideW,"position":"relative","overflow":"hidden","padding":"0","margin":"0","left":-(cloneNum+index*scroll)*slideW}).children().css( {"float":"left"}  ); break;  // width:'+vis*slideW+'px"
+						conBox.wrap('<div class="tempWrap" style="overflow:hidden; position:relative;></div>').css( { "width":(conBoxSize+vis+cloneNum)*slideW,"position":"relative","overflow":"hidden","padding":"0","margin":"0","left":-(cloneNum+index*scroll)*slideW}).children().css( {"float":"left","width":selfW}  ); break;  // width:'+vis*slideW+'px"
 					case "topLoop":
 					case "topMarquee":
 						cloneEle();
