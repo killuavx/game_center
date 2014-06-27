a05a1449069b75a1e56a16b78a1653ff4c5aa571
Killua.VX <killua.vx@gmail.com>
2014-06-27 15:18:50 +0800

    fix uc_client_api.php 中文编码问题

--- webservice/account/uc_client/uc_client_api.php
+++ webservice/account/uc_client/uc_client_api.php
@@ -4,6 +4,48 @@ $PROJECT_ROOT = '/data0/www/bbs';
 include $PROJECT_ROOT . '/config/config_ucenter.php';
 include $PROJECT_ROOT . '/uc_client/client.php';
 
+function safeEncoding($string, $outEncoding='UTF-8')
+{
+    $encoding = "UTF-8";
+    for($i=0;$i<strlen($string);$i++)
+    {
+        if(ord($string{$i})<128)
+            continue;
+
+        if((ord($string{$i})&224)==224)
+        {
+            //第一个字节判断通过
+            $char = $string{++$i};
+            if((ord($char)&128)==128)
+            {
+                //第二个字节判断通过
+                $char = $string{++$i};
+                if((ord($char)&128)==128)
+                {
+                    $encoding = "UTF-8";
+                    break;
+                }
+            }
+        }
+        if((ord($string{$i})&192)==192)
+        {
+            //第一个字节判断通过
+            $char = $string{++$i};
+            if((ord($char)&128)==128)
+            {
+                //第二个字节判断通过
+                $encoding = "GB2312";
+                break;
+            }
+        }
+    }
+
+    if(strtoupper($encoding) == strtoupper($outEncoding))
+        return $string;
+    else
+        return iconv($encoding,$outEncoding,$string);
+}
+
 $func = $argv[1];
 if(!is_callable($func))
 {
@@ -13,6 +55,14 @@ if(!is_callable($func))
 
 $line = trim(fgets(STDIN));
 $func_argv = json_decode($line);
+foreach($func_argv as $key => $val)
+{
+    if(is_string($val))
+    {
+        $func_argv[$key] = safeEncoding($val, 'gbk');
+    }
+}
+
 if(!is_array($func_argv))
 {
   echo json_encode(null);
@@ -20,5 +70,12 @@ if(!is_array($func_argv))
 }
 
 $result = call_user_func_array($func, $func_argv);
+if(is_array($result))
+{
+    for($result as $key => $val)
+    {
+        $result[$key] = safeEncoding($val,'UTF-8');
+    }
+}
 echo json_encode($result);
 exit(0);
