ef451769d55e369351347412bc6b1ec267bda67b
Killua.VX <killua.vx@gmail.com>
2014-06-27 17:13:07 +0800

    清理无用文件

--- webservice/website/widgets/developer.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from django_widgets import Widget
-from .common.category import BaseTopicAuthorPackageListWidget
-
-
-class DeveloperPackageShowcaseWidget(BaseTopicAuthorPackageListWidget, Widget):
-
-    template = 'pages/widgets/developers_package_page.haml'
-
