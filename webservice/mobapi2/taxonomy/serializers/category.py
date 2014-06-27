b433fe221ff7d9ab902c6ae95241723cae395f40
Killua.VX <killua.vx@gmail.com>
2014-06-27 22:40:29 +0800

    重新获取分类列表接口的前几款应用

--- webservice/mobapi2/taxonomy/serializers/category.py
+++ webservice/mobapi2/taxonomy/serializers/category.py
@@ -52,7 +52,7 @@ class CategoryRelatedPackagesMixin(object):
     limit_packages = 4
 
     def get_packages(self, obj):
-        if not obj.children.count():
+        if obj.children.count():
             return list()
         packages = obj.packages.published().by_released_order(True)[0:self.limit_packages]
         return self.serializer_class_package(packages,
