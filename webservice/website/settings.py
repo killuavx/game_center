64e2b166d65234ea931eebd52d36171b908c768e
Killua.VX <killua.vx@gmail.com>
2014-08-18 04:06:59 +0800

    tiny change

--- webservice/website/settings.py
+++ webservice/website/settings.py
@@ -1,10 +1,2 @@
 # -*- coding: utf-8 -*-
-from django.conf import settings
 
-MOGOENGINE_CONNECTS = getattr(settings, 'MOGOENGINE_CONNECTS', dict(
-    systemresource=dict(
-        host='localhost',
-        port=27017,
-        name='systemresource'
-    )
-))
