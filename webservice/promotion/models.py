75a3e906fc3f060350e6e50187df959815ab8381
Killua.VX <killua.vx@gmail.com>
2014-09-01 15:40:27 +0800

    推荐后台管理

--- webservice/promotion/models.py
+++ webservice/promotion/models.py
@@ -14,6 +14,8 @@ from toolkit.managers import CurrentSitePassThroughManager
 from toolkit.helpers import sync_status_from, released_hourly_datetime
 from toolkit import model_url_mixin as urlmixin
 from toolkit.models import SiteRelated
+from toolkit.storage import image_storage
+from toolkit.fields import QiniuThumbnailerImageField
 from django.core import exceptions
 from copy import deepcopy
 
@@ -234,3 +236,170 @@ def advertisement_pre_save(sender, instance, **kwargs):
 
     advertisement_workspace_path(instance, now())
 
+
+from dateutil import rrule
+from datetime import timedelta
+from django.db.models.query import Q
+
+
+class RecommendQuerySet(AdvertisementQuerySet):
+
+    def published(self, released_hourly=True):
+        dt = released_hourly_datetime(now(), released_hourly)
+        return self.filter(status=self.model.STATUS.published)\
+            .filter(Q(released_datetime__lte=dt))\
+            .filter(Q(expiry_datetime__gt=dt) | Q(expiry_datetime=None))
+
+
+RECOMMEND_DIRECTORY_DTFORMAT = 'recommend/%Y/%m/%d/%H%M-%S-%f'
+
+
+def recommend_workspace_path(instance, dt):
+    if not instance.workspace:
+        instance.workspace = dt.astimezone().strftime(RECOMMEND_DIRECTORY_DTFORMAT)
+    return instance
+
+
+def recommend_upload_to(instance, filename):
+    fbasename = basename(filename)
+    if not instance.created_datetime:
+        instance.created_datetime = now().astimezone()
+    recommend_workspace_path(instance, instance.created_datetime)
+    return "%s/%s" %(instance.workspace.name, fbasename.lower())
+
+
+class Recommend(urlmixin.RecommendAbsoluteUrlMixin,
+                SiteRelated, models.Model):
+    """
+    小编推荐
+    """
+
+    class Meta:
+        index_together = (
+            ('site', 'content_type', 'object_id', ),
+            ('site', 'status', ),
+            ('site', 'released_datetime', 'expiry_datetime', ),
+            ('site', 'status', 'released_datetime', 'expiry_datetime', ),
+        )
+
+    objects = CurrentSitePassThroughManager \
+        .for_queryset_class(RecommendQuerySet)()
+
+    title = models.CharField(verbose_name=_('title'), max_length=36, )
+
+    summary = models.CharField(max_length=250)
+
+    cover = QiniuThumbnailerImageField(
+        storage=image_storage,
+        thumbnail_storage=image_storage,
+        upload_to=recommend_upload_to,
+    )
+
+    content = generic.GenericForeignKey("content_type", "object_id")
+    object_id = models.PositiveIntegerField(default=0, blank=True)
+    content_type = models.ForeignKey(ContentType,
+                                     limit_choices_to=dict(app_label='warehouse',
+                                                           model__in=['package', 'packageversion']),
+                                     blank=True,
+                                     null=True)
+
+    workspace = FileField(default='',
+                          blank=True,
+                          max_length=500,
+                          format='File')
+
+    resources = MultiResourceField()
+
+    STATUS = Choices(
+        ('draft', _('Draft')),
+        ('unpublished', _('Unpublished')),
+        ('published', _('Published')),
+    )
+
+    status = StatusField(default=STATUS.draft, blank=True)
+
+    weekday_numbers = models.CharField(max_length=15,
+                                       default='0',
+                                       help_text='一周推荐星期序号, 可多选, monday:0 ... sunday:6, 格式以空格分割'
+    )
+
+    released_datetime = models.DateTimeField(db_index=True)
+
+    updated_datetime = models.DateTimeField(db_index=True, editable=False)
+
+    created_datetime = models.DateTimeField(auto_now_add=now, editable=False)
+
+    expiry_datetime = models.DateTimeField(_("Expires on"),
+                                           help_text=_("With Published chosen, won't be shown after this time"),
+                                           blank=True, null=True)
+
+    tracker = FieldTracker()
+
+    def is_published(self, released_hourly=True):
+        dt = released_hourly_datetime(now(), released_hourly)
+        return self.status == self.STATUS.published \
+            and self.released_datetime <= dt \
+            and self.expiry_datetime > dt
+    is_published.boolean = True
+    is_published.short_description = _('is published?')
+
+    def allow_show(self, dt, released_hourly=True):
+        dt = released_hourly_datetime(dt, released_hourly)
+        return dt.weekday() in [w.weekday for w in self.weekdays]
+
+    def can_show_today(self):
+        return self.allow_show(now().astimezone())
+    can_show_today.boolean = True
+
+    def allow_show_dates(self, max_count=None):
+        expiry_datetime = self.expiry_datetime
+        if not self.expiry_datetime:
+            expiry_datetime = self.released_datetime + timedelta(days=7)
+
+        return rrule.rrule(rrule.WEEKLY,
+                           count=max_count,
+                           byweekday=self.weekdays,
+                           wkst=rrule.SU,
+                           dtstart=self.released_datetime,
+                           until=expiry_datetime)
+
+    @property
+    def weekdays(self):
+        return [rrule.weekdays[int(wd)] for wd in self.weekday_numbers.split(' ')]
+
+    def clean(self):
+        self.updated_datetime = now()
+        self.weekday_numbers = self.weekday_numbers.strip()
+        try:
+            weekdays = self.weekdays
+        except TypeError:
+            e = exceptions.ValidationError(dict(
+                weekdays='weekdays must in %s' % (
+                    {_wd.weekday: repr(_wd) for _wd in rrule.weekdays}
+                )))
+
+    @property
+    def icon(self):
+        obj = self.content
+        if not obj:
+            return None
+        else:
+            # for PackageVersion
+            if hasattr(obj, 'icon'):
+                return obj.icon
+
+            # for Package
+            elif getattr(obj, 'latest_version', None):
+                return obj.latest_version.icon
+        return None
+
+    def save(self, update_site=False, *args, **kwargs):
+        if not self.updated_datetime:
+            self.updated_datetime = self.created_datetime
+        return super(Recommend, self).save(update_site=update_site, *args, **kwargs)
+
+    def sync_status(self):
+        return sync_status_from(self)
+
+    def __str__(self):
+        return self.title
