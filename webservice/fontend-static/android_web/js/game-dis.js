bd5f726e197137f8d839a9a1f96f74316c415de3
Killua.VX <killua.vx@gmail.com>
2014-09-18 16:01:49 +0800

    fix 主站详情页面评论翻页js page_load未定义

--- webservice/fontend-static/android_web/js/game-dis.js
+++ webservice/fontend-static/android_web/js/game-dis.js
@@ -1,7 +1,6 @@
 /*
  * app详情图片事件绑定
 */
-
 var imgloadCount = 0;
 function bindAppDetailCoverEv(vis_count){
 
@@ -32,127 +31,22 @@ function bindAppDetailCoverEv(vis_count){
 	}	
 }
 
-
-
-$(function(){
-	
-	//举报 纠错-表单验证
-	var report=$(".report-form").Validform({
-		showAllError:true,	
-		tiptype:function(msg,o,cssctl){
-			var objtip=$(".Validform_checktip");
-			cssctl(objtip,o.type);
-			objtip.text(msg);
-		}
-	});	
-	report.addRule([{
-		ele:".fbkcontent",datatype:"*"}
-	]);
-	
-	
-	//举报 纠错
-		$(".report").click(function(){
-			$(".report-box").zxxbox({
-				title: "虫虫游戏问题反馈"	,fix: true, bgclose:true
-				});
-		});
-	
-	
-	
-	//评论
-    var comment_tips = $('#comments .comment-tip');
-    var review=$(".comment-form").Validform({
-        showAllError:true,
-        tiptype:function(msg,o,cssctl){
-            var objtip=$("");
-            cssctl(comment_tips,o.type);
-            objtip.text(msg);
-        },
-        ajaxPost:true,
-        callback: function(data){
-            if( data.code == 0 )
-            {
-                comment_tips.html('');
-                page_load('#comment-list', $('#comment-list .page'), 1);
-                $('.comment-form textarea[name=comment]').val('');
-                $('.comment-form input[name=rating_output]').val('');
-                $('#rating_on').attr('style', '');
-                alert(data.msg);
-            }
-            else
-            {
-                var msgs = [];
-                for(k in data.errors)
-                {
-                    msgs.push(data.errors[k].join(','));
-                }
-                var _msg = data.msg + " " + msgs.join(", ")
-                comment_tips.html(_msg);
-            }
-            return false;
+function page_load(load_box, dom, p){
+    var _load_box = $(load_box);
+    var url = $(dom).attr('data-url');
+    if( !url ){
+          return false
+          }
+    $.ajax({
+        type:'get',
+        url:url,
+        data:{
+         page:p
+         },
+        dataType:'html',
+        success:function(data){
+            _load_box.html(data);
         }
     });
-	review.addRule([{
-		ele:".comment-box",datatype:"*1-300"}
-	]);	
-	
-	
-	
-	
-});
-
-
-//评分
-$(document).ready(function(){
-	$('#rating_btns li').hover(function(){	
-			$rating = $(this).text();
-			$('#rating_on').css('width', rateWidth($rating));
-	},function(){
-		 $rating = $('#rating').text();
-		if($rating == ""){		
-			 $('#rating_on').css('width', "0px");
-		 }
-		 else{
-			 $('#rating_on').css('width', rateWidth($rating));	
-		 }
-	});
-	$('#rating_btns li').click(function(){
-		$rating = $(this).text();			
-		$('#rating').text($rating+'分');
-		$('#rating_output').val($rating);
-	});	
-	function rateWidth($rating){		
-		$rating = parseFloat($rating);
-		switch ($rating){
-			case 1: $width = "25px"; break;
-			case 2: $width = "54px"; break;
-			case 3: $width = "81px"; break;
-			case 4: $width = "107px"; break;
-			case 5: $width = "134px"; break;
-			default:  $width =  "0";
-		}
-		return $width;
-	}
-});	
-$(document).ready(function(){
-    function page_load(load_box, dom, p){
-        var _load_box = $(load_box);
-        var url = $(dom).attr('data-url');
-        if( !url ){
-            return false
-        }
-        $.ajax({
-            type:'get',
-            url:url,
-            data:{
-                page:p
-            },
-            dataType:'html',
-            success:function(data){
-                _load_box.html(data);
-            }
-        });
-        return false;
-    }
-    window.page_load = page_load;
-});
+    return false;
+}
