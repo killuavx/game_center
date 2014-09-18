276a867822d4017f5b59c224f1e2a1f2d28a34d3
Killua.VX <killua.vx@gmail.com>
2014-09-18 10:33:23 +0800

    用户上传图标，即刻同步CDN

---
+++ webservice/website/cdn/processors/account.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from os.path import dirname
+from website.cdn.errors import WorkingDirectoryNotFound
+from .base import ModelProcessor
+
+
+class ProfileProcessor(ModelProcessor):
+
+    def __init__(self, instance):
+        super(ProfileProcessor, self).__init__(instance)
+        self.relative_path = self.get_relative_working_path()
+
+    def get_relative_working_path(self):
+        if self.instance.workspace:
+            return str(self.instance.workspace)
+        try:
+            self.instance.icon.size
+        except FileNotFoundError:
+            path = self.instance.icon.path
+            raise WorkingDirectoryNotFound(
+                "working direcotry %s '%s' not found" % (self.instance, path))
+        return dirname(self.instance.icon.name)
