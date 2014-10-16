a94834ac7c19cff9cf7b447fbc70953e674790bc
Killua.VX <killua.vx@gmail.com>
2014-10-16 17:30:59 +0800

    mobapi2详情接口添加reported信息

--- webservice/mobapi2/warehouse/serializers/package.py
+++ webservice/mobapi2/warehouse/serializers/package.py
@@ -14,6 +14,34 @@ from mobapi2.warehouse.serializers.mixin import (
 from mobapi2.serializers import HyperlinkedModelSerializer
 
 
+PACKAGE_SUMMARY_FIELDS = (
+    'url',
+    'icon',
+    'cover',
+    'package_name',
+    'title',
+    'has_award',
+    'award_coin',
+    'flags',
+    'tags',
+    'star',
+    'category_name',
+    'categories_names',
+    'version_count',
+    'summary',
+    'author',
+    'download_type',
+    'download',
+    'download_size',
+    'download_count',
+    'comments_url',
+    'released_datetime',
+    'actions',
+    'version_name',
+    'version_code',
+    'versions_url',)
+
+
 class PackageSummarySerializer(PackageRelatedVersionsMixin,
                                PackageRelatedLatestVersinoMixin,
                                PackageRelatedCategoryMixin,
@@ -52,125 +80,51 @@ class PackageSummarySerializer(PackageRelatedVersionsMixin,
 
     class Meta:
         model = Package
-        fields = ('url',
-                  'icon',
-                  'cover',
-                  'package_name',
-                  'title',
-                  'has_award',
-                  'award_coin',
-                  'flags',
-                  'tags',
-                  'star',
-                  'category_name',
-                  'categories_names',
-                  'version_count',
-                  'summary',
-                  'author',
-                  'download_type',
-                  'download',
-                  'download_size',
-                  'download_count',
-                  'comments_url',
-                  'released_datetime',
-                  'actions',
-                  'version_name',
-                  'version_code',
-                  'versions_url',
-        )
+        fields = PACKAGE_SUMMARY_FIELDS
+
+PACKAGE_DETAIL_FIELDS = PACKAGE_SUMMARY_FIELDS + (
+    'whatsnew',
+    'summary',
+    'description',
+    'download_count',
+    'comment_count',
+    'stars_good_rate',
+    'stars_medium_rate',
+    'stars_low_rate',
+    'screenshots',
+    'related_packages_url',
+    'versions_url',
+    'supported_languages',
+    'reported',
+)
 
 
-
-class PackageDetailSerializer(PackageRelatedLatestVersinoMixin,
-                              PackageRelatedVersionsMixin,
-                              PackageRelatedCategoryMixin,
-                              PackageRelatedTagMin,
-                              PackageActionsMixin,
-                              PackageRelatedAuthorMixin,
-                              PackageRelatedPackageUrlMixin,
-                              HyperlinkedModelSerializer):
+class PackageDetailSerializer(PackageRelatedPackageUrlMixin,
+                              PackageSummarySerializer):
 
     entrytype = 'client'
 
-    icon = serializers.SerializerMethodField('get_latest_version_icon_url')
-    cover = serializers.SerializerMethodField('get_latest_version_cover_url')
-    title = serializers.SerializerMethodField('get_latest_version_title')
-    version_name = serializers.SerializerMethodField('get_latest_version_name')
-    version_code = serializers.SerializerMethodField('get_latest_version_code')
     whatsnew = serializers.SerializerMethodField('get_latest_version_whatsnew')
+
     screenshots = serializers.SerializerMethodField(
         'get_latest_version_screenshots')
-    category_name = serializers.SerializerMethodField('get_main_category_name')
-    categories_names = serializers.SerializerMethodField('get_categories_names')
-    download_type = serializers.SerializerMethodField('get_latest_version_download_type')
-    download = serializers.SerializerMethodField('get_latest_version_download')
     download_count = serializers.SerializerMethodField(
         'get_latest_version_download_count')
-    download_size = serializers.SerializerMethodField(
-        'get_latest_version_download_size')
     comment_count = serializers.SerializerMethodField(
         'get_latest_version_comment_count')
-    comments_url = serializers.SerializerMethodField(
-        'get_latest_version_comments_url')
-    tags = serializers.SerializerMethodField('get_tags')
 
-    star = serializers.SerializerMethodField('get_latest_version_star')
     stars_good_rate = serializers.SerializerMethodField('get_latest_version_stars_good_rate')
     stars_medium_rate = serializers.SerializerMethodField('get_latest_version_stars_medium_rate')
     stars_low_rate = serializers.SerializerMethodField('get_latest_version_stars_low_rate')
 
-    actions = serializers.SerializerMethodField('get_action_links')
-
-    author = serializers.SerializerMethodField('get_author')
-
     related_packages_url = serializers.SerializerMethodField('get_related_packages_url')
-
-    versions_url = serializers.SerializerMethodField('get_versions_url')
-
     supported_languages = serializers.SerializerMethodField('get_latest_version_supported_languages')
 
-    has_award = serializers.SerializerMethodField('get_latest_version_has_award')
-
-    award_coin = serializers.SerializerMethodField('get_latest_version_award_coin')
-
-    flags = serializers.SerializerMethodField('get_latest_version_flags')
+    reported = serializers.SerializerMethodField('get_latest_version_reported')
 
     class Meta:
         model = Package
-        fields = ('url',
-                  'icon',
-                  'cover',
-                  'package_name',
-                  'title',
-                  'version_code',
-                  'version_name',
-                  'download_type',
-                  'download',
-                  'download_count',
-                  'download_size',
-                  'comment_count',
-                  'comments_url',
-                  'star',
-                  'stars_good_rate',
-                  'stars_medium_rate',
-                  'stars_low_rate',
-                  'flags',
-                  'tags',
-                  'category_name',
-                  'categories_names',
-                  'whatsnew',
-                  'summary',
-                  'description',
-                  'author',
-                  'released_datetime',
-                  'screenshots',
-                  'actions',
-                  'versions_url',
-                  'related_packages_url',
-                  'supported_languages',
-                  'has_award',
-                  'award_coin',
-        )
+        fields = PACKAGE_DETAIL_FIELDS
 
 
 class PackageUpdateSummarySerializer(PackageSummarySerializer):
@@ -211,27 +165,6 @@ class PackageUpdateSummarySerializer(PackageSummarySerializer):
 class PackageSummaryWithMyCommentSerializer(PackageSummarySerializer):
     entrytype = 'client'
 
-    icon = serializers.SerializerMethodField('get_latest_version_icon_url')
-    cover = serializers.SerializerMethodField('get_latest_version_cover_url')
-    title = serializers.SerializerMethodField('get_latest_version_title')
-    category_name = serializers.SerializerMethodField('get_main_category_name')
-    categories_names = serializers.SerializerMethodField('get_categories_names')
-    version_count = serializers.SerializerMethodField('get_version_count')
-    download = serializers.SerializerMethodField(
-        'get_latest_version_download')
-    download_size = serializers.SerializerMethodField(
-        'get_latest_version_download_size')
-    comments_url = serializers.SerializerMethodField(
-        'get_latest_version_comments_url')
-    actions = serializers.SerializerMethodField('get_action_links')
-    tags = serializers.SerializerMethodField('get_tags')
-    star = serializers.SerializerMethodField('get_latest_version_star')
-
-    author = serializers.SerializerMethodField('get_author')
-    version_name = serializers.SerializerMethodField('get_latest_version_name')
-    version_code = serializers.SerializerMethodField('get_latest_version_code')
-    versions_url = serializers.SerializerMethodField('get_versions_url')
-
     def _get_comment(self, obj):
         request = self.context.get('request')
         qs = self.get_latest_version_comment(obj).filter(user=request.user)
@@ -254,40 +187,9 @@ class PackageSummaryWithMyCommentSerializer(PackageSummarySerializer):
         except (AttributeError, ObjectDoesNotExist) as e:
             return None
 
-    flags = serializers.SerializerMethodField('get_latest_version_flags')
-
-    has_award = serializers.SerializerMethodField('get_latest_version_has_award')
-
-    award_coin = serializers.SerializerMethodField('get_latest_version_award_coin')
 
     class Meta:
         model = Package
-        fields = ('url',
-                  'icon',
-                  'cover',
-                  'package_name',
-                  'title',
-                  'flags',
-                  'tags',
-                  'star',
-                  'category_name',
-                  'categories_names',
-                  'version_count',
-                  'summary',
-                  'author',
-                  'download',
-                  'download_size',
-                  'download_count',
-                  'comments_url',
-                  'released_datetime',
-                  'actions',
-                  'version_name',
-                  'version_code',
-                  'versions_url',
-                  'comment',
-                  'submit_date',
-                  'has_award',
-                  'award_coin',
-        )
+        fields =PACKAGE_SUMMARY_FIELDS + ('comment', 'submit_date',)
 
 
